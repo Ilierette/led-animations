375529f80544aedc2150d25ccccf4dc69570bdf9
Karolina Pietraś <Ilierette@users.noreply.github.com>
2023-05-24 22:12:22 +0200

    dice breathe

--- dice/code.py
+++ dice/code.py
@@ -4,8 +4,19 @@ import neopixel
 
 pixel_pin = board.A3
 num_pixels = 7
+once_run = True
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1.0, auto_write=False)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1.0, auto_write=True)
+
+
+def breatheIn(sleep):
+    for i in range(30,255):
+        pixels.fill((0,i,i))
+    time.sleep(sleep)
+
+def breatheOut():
+    for i in reversed(range(30,255)):
+        pixels.fill((0,i,i))
 
 def color_chase(color, wait):
     for i in range(num_pixels):
@@ -14,14 +25,17 @@ def color_chase(color, wait):
         pixels.show()
     time.sleep(0.5)
 
-
-
-YELLOW = (255, 150, 0)
-PURPLE = (180, 0, 255)
-CYAN = (0, 255, 255)
+ORANGE = (255,60,0)
+PURPLE = (90, 0, 255)
+OFF = (0,0,0)
 
 while True:
-    color_chase(YELLOW, 0.1)
-    color_chase(CYAN, 0.1)
-    color_chase(PURPLE, 0.1)
+    if(once_run):
+        color_chase(PURPLE, 0.1)
+        color_chase(ORANGE, 0.1)
+        color_chase(OFF, 0.1)
+        once_run = False
 
+    breatheIn(0.6)
+    breatheOut()
+    
